ad6a463491799bfb8378ed46d2524f0b25612748
lky5645 <563316700@qq.com>
2020-04-17 12:27:56 +0800

    新增配置中心子模块

---
+++ cloud-provider-config3344/src/main/java/com/ts/provider/ConfigEurekaMain3344.java
@@ -0,0 +1,20 @@
+package com.ts.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName ConfigEurekaMain
+ * @Description TODO
+ * @Author 86175
+ * @Date 2020/4/17 10:23
+ * @Params TODO
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigEurekaMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigEurekaMain3344.class,args);
+    }
+}
